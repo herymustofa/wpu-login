43be257c3d8423cfbaa1253f3b8afcf6c09922ed
herymustofa <herymustofa@gmail.com>
2019-05-23 14:07:15 +0700

    edit controller/auth and add views/login.php

--- application/controllers/auth.php
+++ application/controllers/auth.php
@@ -1,8 +1,10 @@
 <?php
-defined('BASEPATH') OR exit('No direct script access allowed');
+defined('BASEPATH') or exit('No direct script access allowed');
 
-class Auth extends CI_Controller {
-    public function index() {
-        echo 'auth/index';
+class Auth extends CI_Controller
+{
+    public function index()
+    {
+        $this->load->view('auth/login');
     }
 }
