1d1a3b6847881c62a07a0a84d9b7ad86651c82a6
herymustofa <herymustofa@gmail.com>
2019-05-23 14:48:13 +0700

    menambah aut.php aut_footer.php dan auth_header.php

--- application/controllers/auth.php
+++ application/controllers/auth.php
@@ -5,6 +5,8 @@ class Auth extends CI_Controller
 {
     public function index()
     {
+        $this->load->view('templates/auth_header');
         $this->load->view('auth/login');
+        $this->load->view('templates/auth_footer');
     }
 }
