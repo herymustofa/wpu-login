8d4edd17e203271742bd73be58ac681c4539d511
herymustofa <herymustofa@gmail.com>
2019-07-02 13:31:48 +0700

    pertemuan 10, upload gambar

--- application/controllers/User.php
+++ application/controllers/User.php
@@ -27,10 +27,53 @@ class User extends CI_Controller
         $data['user'] = $this->db->get_where('user', ['email' => $this->session->userdata('email')])->row_array();
         //var_dump($data);
         //echo "selamat datang ". $data['user']['name'];
-        $this->load->view('templates/header', $data);
-        $this->load->view('templates/sidebar', $data);
-        $this->load->view('templates/topbar', $data);
-        $this->load->view('user/edit', $data);
-        $this->load->view('templates/footer');
+        $this->form_validation->set_rules('name', 'Full Name', 'required|trim');
+        if ($this->form_validation->run() == false) {
+
+            $this->load->view('templates/header', $data);
+            $this->load->view('templates/sidebar', $data);
+            $this->load->view('templates/topbar', $data);
+            $this->load->view('user/edit', $data);
+            $this->load->view('templates/footer');
+        } else {
+            $name = $this->input->post('name');
+            $email = $this->input->post('email');
+
+            //cek jika ada gambar yang akan di upload
+            $upload_image = $_FILES['image']['name'];
+            // var_dump($upload_image);
+            // die;
+
+            if ($upload_image) {
+                $config['upload_path'] = './assets/img/profile/';
+                $config['allowed_types'] = 'gif|jpg|png';
+                $config['max_size']     = '2048';
+
+                //$config['max_width'] = '1024';
+                //$config['max_height'] = '768';
+                $this->load->library('upload', $config);
+
+                if ($this->upload->do_upload('image')) {
+                    $old_image = $data['user']['image'];
+                    if ($old_image !=  'default1.jpg') {
+                        unlink(FCPATH . 'assets/img/profile/' . $old_image);
+                    }
+
+                    $new_image = $this->upload->data('file_name');
+                    // var_dump($new_image);
+                    // die;
+                    $this->db->set('image', $new_image);
+                } else {
+                    echo $this->upload->display_errors();
+                }
+            }
+
+            $this->db->set('name', $name);
+            $this->db->where('email', $email);
+            $this->db->update('user');
+
+            $this->session->set_flashdata('message', '<div class="alert alert-success" role="alert">Your profile has been updated</div>');
+            redirect('user');
+        }
     }
 }
--- application/views/user/edit.php
+++ application/views/user/edit.php
@@ -4,19 +4,46 @@
     <!-- Page Heading -->
     <h1 class="h3 mb-4 text-gray-800"><?= $title ?></h1>
 
-    <div class="card mb-3" style="max-width: 540px;">
-        <div class="row no-gutters">
-            <div class="col-md-4">
-                <img src="<?= base_url('assets/img/profile/') . $user['image'] ?>" class="card-img" alt="...">
-            </div>
-            <div class="col-md-8">
-                <div class="card-body">
-                    <h5 class="card-title"><?= $user['name'] ?></h5>
-                    <p class="card-text"><?= $user['email'] ?></p>
-                    <p class="card-text"><small class="text-muted">Role ID : <?= $user['role_id'] ?> [1 : admin] [2 : user]</small></p>
-                    <p class="card-text"><small class="text-muted">Member since <?= date('d F Y', $user['date_created']) ?> </small></p>
+    <div class="row">
+        <div class="col-lg-8">
+            <?= form_open_multipart('user/edit'); ?>
+            <div class="form-group row">
+                <label for="email" class="col-sm-2 col-form-label">Email</label>
+                <div class="col-sm-10">
+                    <input type="email" class="form-control" id="email" placeholder="Email" name="email" value="<?= $user['email'] ?>" readonly>
                 </div>
             </div>
+            <div class="form-group row">
+                <label for="name" class="col-sm-2 col-form-label">Full Name</label>
+                <div class="col-sm-10">
+                    <input type="text" class="form-control" id="name" placeholder="Name" name="name" value="<?= $user['name'] ?>">
+                    <?= form_error('name', '<small class="text-danger pl-3">', '</small>') ?>
+                </div>
+            </div>
+            <div class="form-group row">
+                <div class="col-sm-2">Picture</div>
+                <div class="col-sm-10">
+                    <div class="row">
+                        <div class="col-sm-3">
+                            <img src="<?= base_url('assets/img/profile/') . $user['image'] ?>" class="img-thumbnail">
+                        </div>
+                        <div class="col-sm-9">
+                            <div class="custom-file">
+                                <input type="file" class="custom-file-input" id="image" name="image">
+                                <label class="custom-file-label" for="image">Choose file</label>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="form-group row justify-content-end">
+                <div class="col-sm-10">
+                    <button type="submit" class="btn btn-primary">Edit </button>
+                </div>
+            </div>
+
+
+            </form>
         </div>
     </div>
 
